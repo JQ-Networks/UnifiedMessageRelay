3fdd361728c1324e497327e8fd8229776e05f021
jqqqqqqqqqq <jqqqqqqqqqq@gmail.com>
2017-07-29 18:48:37 +0800

    add config samples

---
+++ bot_constant-sample.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+tgToken = 'your telegram bot token'
+tgBotId = 12345678 # tg bot id
+qqBotId = 12345678 # qq number
+forwardIds = [(12345678, -12345678), (87654321, -87654321)]  # (qq group id, tg group id)
+server_pic_url = 'http://example.com:8080/image/'
