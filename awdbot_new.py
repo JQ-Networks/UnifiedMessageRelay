6dc5f8271abe3b2d33bdb68144073433bfb13fd4
jqqqqqqqqqq <jqqqqqqqqqq@gmail.com>
2017-08-01 23:27:39 +0800

    bugfix: no start main
    # Conflicts:
    #	awdbot.py

--- awdbot_new.py
+++ awdbot_new.py
@@ -417,6 +417,7 @@ def main():
     qq_bot.start()
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
+
     dp.add_handler(MessageHandler(Filters.text, text_from_telegram))
     dp.add_handler(MessageHandler(Filters.sticker, sticker_from_telegram))
     dp.add_handler(MessageHandler(Filters.audio, audio_from_telegram))
@@ -432,3 +433,6 @@ def main():
     # SIGTERM or SIGABRT. This should be used most of the time, since
     # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
+
+if __name__ == '__main__':
+    main()
