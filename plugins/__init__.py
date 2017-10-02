7ed42c7041bc272de4fb5cd4920357a54b4f81c0
jqqqqqqqqqq <jqqqqqqqqqq@qq.com>
2017-10-03 00:23:09 +0800

    telegram bot framework
    minor modifications which enable multiple python clients

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,11 +1,17 @@
 import automodinit
 import os
 
-__all__ = []
-for _module in os.listdir(os.path.dirname(__file__)):
-    if _module == '__init__.py' or _module[-3:] != '.py':
-        continue
-    __all__.append(_module[:-3])
-del os
+# __all__ = []
+# for _module in os.listdir(os.path.dirname(__file__)):
+#     if _module == '__init__.py' or _module[-3:] != '.py':
+#         continue
+#     __all__.append(_module[:-3])
+# del os
+# automodinit.automodinit(__name__, __file__, globals())
+# del automodinit
+
+__all__ = ["I will get rewritten"]
+# Don't modify the line above, or this line!
+import automodinit
 automodinit.automodinit(__name__, __file__, globals())
 del automodinit
