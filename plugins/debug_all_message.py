c36b41c1e13ad8c8c4aca4c303ce6bd0335fe7d3
jqqqqqqqqqq <jqqqqqqqqqq@qq.com>
2017-10-05 20:09:23 +0800

    telegram bot framework 2.0
    add listen all debugger

--- plugins/debug_all_message.py
+++ plugins/debug_all_message.py
@@ -3,20 +3,95 @@ from cqsdk import *
 
 
 @global_vars.qq_bot.listener((RcvdPrivateMessage, ), 1)  # priority 1
+def test(message):
+    print(message)
+    return False
+
+
 @global_vars.qq_bot.listener((RcvdGroupMessage, ), 1)  # priority 1
+def test(message):
+    print(message)
+    return False
+
+
 @global_vars.qq_bot.listener((RcvdDiscussMessage, ), 1)  # priority 1
+def test(message):
+    print(message)
+    return False
+
+
 @global_vars.qq_bot.listener((GroupAdminChange, ), 1)  # priority 1
+def test(message):
+    print(message)
+    return False
+
+
 @global_vars.qq_bot.listener((GroupMemberDecrease, ), 1)  # priority 1
+def test(message):
+    print(message)
+    return False
+
+
 @global_vars.qq_bot.listener((GroupMemberIncrease, ), 1)  # priority 1
+def test(message):
+    print(message)
+    return False
+
+
 @global_vars.qq_bot.listener((FriendAdded, ), 1)  # priority 1
+def test(message):
+    print(message)
+    return False
+
+
 @global_vars.qq_bot.listener((GroupUpload, ), 1)  # priority 1
+def test(message):
+    print(message)
+    return False
+
+
 @global_vars.qq_bot.listener((RcvGroupMemberInfo, ), 1)  # priority 1
+def test(message):
+    print(message)
+    return False
+
+
 @global_vars.qq_bot.listener((RcvGroupMemberList, ), 1)  # priority 1
+def test(message):
+    print(message)
+    return False
+
+
 @global_vars.qq_bot.listener((RcvStrangerInfo, ), 1)  # priority 1
+def test(message):
+    print(message)
+    return False
+
+
 @global_vars.qq_bot.listener((RcvCookies, ), 1)  # priority 1
+def test(message):
+    print(message)
+    return False
+
+
 @global_vars.qq_bot.listener((RcvCsrfToken, ), 1)  # priority 1
+def test(message):
+    print(message)
+    return False
+
+
 @global_vars.qq_bot.listener((RcvLoginQQ, ), 1)  # priority 1
+def test(message):
+    print(message)
+    return False
+
+
 @global_vars.qq_bot.listener((RcvLoginNickname, ), 1)  # priority 1
+def test(message):
+    print(message)
+    return False
+
+
 @global_vars.qq_bot.listener((Fatal, ), 1)  # priority 1
 def test(message):
     print(message)
