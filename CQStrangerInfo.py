c5f3fe9195e18150a15bca8ee33bfc7488150ae5
jqqqqqqqqqq <jqqqqqqqqqq@qq.com>
2017-10-05 23:26:51 +0800

    telegram bot framework 2.0
    testing purpose only

--- CQStrangerInfo.py
+++ CQStrangerInfo.py
@@ -9,7 +9,7 @@ class CQStrangerInfo(object):
     Age = None
 
     def __init__(self, data, is_base64=True):
-        data = base64.b64decode(data) if is_base64 else data
+        data = base64.decodebytes(data.encode()) if is_base64 else data
         info = CQUnpack(data)
         self.QQID = info.get_long()
         self.Nickname = info.get_length_str().decode('gb18030')
