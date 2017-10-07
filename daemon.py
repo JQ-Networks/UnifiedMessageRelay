b5af831633b59e638a2278f94da4419881c66a90
jqqqqqqqqqq <jqqqqqqqqqq@qq.com>
2017-10-07 22:26:10 +0800

    telegram bot framework 2.0
    bug fix, sface & bface, cq_rich contains no url

--- daemon.py
+++ daemon.py
@@ -54,13 +54,15 @@ def main():
             daemon.stop()
         elif 'restart' == sys.argv[1]:
             daemon.restart()
+        elif 'run' == sys.argv[1]:
+            daemon.run()
         else:
             print('error processing command')
-            print("usage: %s start|stop|restart" % sys.argv[0])
+            print("usage: %s start|stop|restart|run" % sys.argv[0])
             sys.exit(2)
         sys.exit(0)
     else:
-        print("usage: %s start|stop|restart" % sys.argv[0])
+        print("usage: %s start|stop|restart|run" % sys.argv[0])
         sys.exit(2)
 
 
