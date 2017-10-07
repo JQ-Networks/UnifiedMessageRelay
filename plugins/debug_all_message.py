14bd5ab82f53ad45bfee12e5b81837c07d317305
jqqqqqqqqqq <jqqqqqqqqqq@qq.com>
2017-10-07 22:28:42 +0800

    telegram bot framework 2.0
    bug fix, sface & bface, cq_rich contains no url

--- plugins/debug_all_message.py
+++ plugins/debug_all_message.py
@@ -6,14 +6,14 @@ from bot_constant import FORWARD_LIST
 import base64
 import logging
 
-logging.basicConfig(level=logging.DEBUG,
+logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
                     datefmt='%a, %d %b %Y %H:%M:%S',
                     filename='bot.log',
                     filemode='a')
 
 console = logging.StreamHandler()
-console.setLevel(logging.DEBUG)
+console.setLevel(logging.INFO)
 formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
 console.setFormatter(formatter)
 logging.getLogger('').addHandler(console)
@@ -24,7 +24,7 @@ def test(message):
     qq_group_id = int(message.group)
     _, tg_group_id, forward_index = get_forward_index(qq_group_id=qq_group_id)
     if 'Test_mode' in FORWARD_LIST[forward_index] and FORWARD_LIST[forward_index]['Test_mode']:
-        logging.debug(message)
+        logging.info(message)
     return False
 
 
@@ -33,9 +33,9 @@ def test(message: RcvdGroupMessage):
     qq_group_id = int(message.group)
     _, tg_group_id, forward_index = get_forward_index(qq_group_id=qq_group_id)
     if 'Test_mode' in FORWARD_LIST[forward_index] and FORWARD_LIST[forward_index]['Test_mode']:
-        logging.debug(message)
+        logging.info(message)
     # if message.from_anonymous:
-    #     logging.debug(CQAnonymousInfo(base64.b64decode(message.from_anonymous)))
+    #     logging.info(CQAnonymousInfo(base64.b64decode(message.from_anonymous)))
     return False
 
 
@@ -44,7 +44,7 @@ def test(message):
     qq_group_id = int(message.group)
     _, tg_group_id, forward_index = get_forward_index(qq_group_id=qq_group_id)
     if 'Test_mode' in FORWARD_LIST[forward_index] and FORWARD_LIST[forward_index]['Test_mode']:
-        logging.debug(message)
+        logging.info(message)
     return False
 
 
@@ -53,7 +53,7 @@ def test(message):
     qq_group_id = int(message.group)
     _, tg_group_id, forward_index = get_forward_index(qq_group_id=qq_group_id)
     if 'Test_mode' in FORWARD_LIST[forward_index] and FORWARD_LIST[forward_index]['Test_mode']:
-        logging.debug(message)
+        logging.info(message)
     return False
 
 
@@ -62,7 +62,7 @@ def test(message):
     qq_group_id = int(message.group)
     _, tg_group_id, forward_index = get_forward_index(qq_group_id=qq_group_id)
     if 'Test_mode' in FORWARD_LIST[forward_index] and FORWARD_LIST[forward_index]['Test_mode']:
-        logging.debug(message)
+        logging.info(message)
     return False
 
 
@@ -71,7 +71,7 @@ def test(message):
     qq_group_id = int(message.group)
     _, tg_group_id, forward_index = get_forward_index(qq_group_id=qq_group_id)
     if 'Test_mode' in FORWARD_LIST[forward_index] and FORWARD_LIST[forward_index]['Test_mode']:
-        logging.debug(message)
+        logging.info(message)
     return False
 
 
@@ -80,7 +80,7 @@ def test(message):
     qq_group_id = int(message.group)
     _, tg_group_id, forward_index = get_forward_index(qq_group_id=qq_group_id)
     if 'Test_mode' in FORWARD_LIST[forward_index] and FORWARD_LIST[forward_index]['Test_mode']:
-        logging.debug(message)
+        logging.info(message)
     return False
 
 
@@ -89,7 +89,7 @@ def test(message):
     qq_group_id = int(message.group)
     _, tg_group_id, forward_index = get_forward_index(qq_group_id=qq_group_id)
     if 'Test_mode' in FORWARD_LIST[forward_index] and FORWARD_LIST[forward_index]['Test_mode']:
-        logging.debug(message)
+        logging.info(message)
     return False
 
 
@@ -98,7 +98,7 @@ def test(message):
     qq_group_id = int(message.group)
     _, tg_group_id, forward_index = get_forward_index(qq_group_id=qq_group_id)
     if 'Test_mode' in FORWARD_LIST[forward_index] and FORWARD_LIST[forward_index]['Test_mode']:
-        logging.debug(message)
+        logging.info(message)
     return False
 
 
@@ -107,7 +107,7 @@ def test(message):
     qq_group_id = int(message.group)
     _, tg_group_id, forward_index = get_forward_index(qq_group_id=qq_group_id)
     if 'Test_mode' in FORWARD_LIST[forward_index] and FORWARD_LIST[forward_index]['Test_mode']:
-        logging.debug(message)
+        logging.info(message)
     return False
 
 
@@ -116,7 +116,7 @@ def test(message):
     qq_group_id = int(message.group)
     _, tg_group_id, forward_index = get_forward_index(qq_group_id=qq_group_id)
     if 'Test_mode' in FORWARD_LIST[forward_index] and FORWARD_LIST[forward_index]['Test_mode']:
-        logging.debug(message)
+        logging.info(message)
     return False
 
 
@@ -125,7 +125,7 @@ def test(message):
     qq_group_id = int(message.group)
     _, tg_group_id, forward_index = get_forward_index(qq_group_id=qq_group_id)
     if 'Test_mode' in FORWARD_LIST[forward_index] and FORWARD_LIST[forward_index]['Test_mode']:
-        logging.debug(message)
+        logging.info(message)
     return False
 
 
@@ -134,7 +134,7 @@ def test(message):
     qq_group_id = int(message.group)
     _, tg_group_id, forward_index = get_forward_index(qq_group_id=qq_group_id)
     if 'Test_mode' in FORWARD_LIST[forward_index] and FORWARD_LIST[forward_index]['Test_mode']:
-        logging.debug(message)
+        logging.info(message)
     return False
 
 
@@ -143,7 +143,7 @@ def test(message):
     qq_group_id = int(message.group)
     _, tg_group_id, forward_index = get_forward_index(qq_group_id=qq_group_id)
     if 'Test_mode' in FORWARD_LIST[forward_index] and FORWARD_LIST[forward_index]['Test_mode']:
-        logging.debug(message)
+        logging.info(message)
     return False
 
 
@@ -152,6 +152,6 @@ def test(message):
     qq_group_id = int(message.group)
     _, tg_group_id, forward_index = get_forward_index(qq_group_id=qq_group_id)
     if 'Test_mode' in FORWARD_LIST[forward_index] and FORWARD_LIST[forward_index]['Test_mode']:
-        logging.debug(message)
+        logging.info(message)
     return False
 
