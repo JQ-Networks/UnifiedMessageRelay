c71a8c78c0a701e2a708a7cecb3b122e75ba7ead
jqqqqqqqqqq <jqqqqqqqqqq@qq.com>
2017-11-05 13:12:19 +0800

    telegram bot framework 2.1
    add text filter function in drive mode
    add private chat function

--- daemon.py
+++ daemon.py
@@ -40,7 +40,7 @@ class MainProcess(Daemon):
         dp.add_error_handler(error)
         dp.add_handler(CommandHandler('start', start), group=0)
 
-        logger.info(os.getcwd())
+        logger.warn(os.getcwd())
 
         qq_bot.start()  # start bot before add handler, in order to execute init correctly
         updater.start_polling(poll_interval=1.0, timeout=200)
