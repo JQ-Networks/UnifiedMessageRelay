d0feabff1a49b8bc8abdc78c1bebe0840410f26a
jqqqqqqqqqq <jqqqqqqqqqq@qq.com>
2017-12-07 23:32:39 +0800

    inital release

--- plugins/_005_drive_mode.py
+++ plugins/_005_drive_mode.py
@@ -31,7 +31,7 @@ def tg_drive_mode(bot, update):
 global_vars.dp.add_handler(MessageHandler(Filters.all, tg_drive_mode), get_plugin_priority(__name__))
 
 
-@global_vars.qq_bot.on_message('group', 'discuss', group=get_plugin_priority(__name__))
+@global_vars.qq_bot.on_message(group=get_plugin_priority(__name__), 'group', 'discuss')
 def qq_drive_mode(context: dict):
     qq_group_id = context.get('group_id')
     qq_discuss_id = context.get('discuss_id')
