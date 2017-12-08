4d499892248b850e92dc3be58d45911eea184e9f
jqqqqqqqqqq <jqqqqqqqqqq@qq.com>
2017-12-08 11:16:59 +0800

    fix dice & rps

--- plugins/for_fun.py
+++ plugins/for_fun.py
@@ -15,7 +15,7 @@ def dice(tg_group_id: int, tg_user: telegram.User, tg_message_id: int):
     reply_entity.append({'data': {'text': 'threw a dice'}, 'type': 'text'})
     reply_entity.append({'type': 'dice'})
 
-    send_all_except_current(forward_index, reply_entity, tg_group_id=tg_group_id)
+    send_all_except_current(forward_index, reply_entity, tg_group_id=tg_group_id, tg_user=tg_user)
 
 
 @command_listener('rps', 'rps', tg_only=True, description='rock paper stone')
@@ -28,5 +28,5 @@ def rps(tg_group_id: int, tg_user: telegram.User, tg_message_id: int):
     reply_entity.append({'data': {'text': 'played rock–paper–scissors'}, 'type': 'text'})
     reply_entity.append({'type': 'rps'})
 
-    send_all_except_current(forward_index, reply_entity, tg_group_id=tg_group_id)
+    send_all_except_current(forward_index, reply_entity, tg_group_id=tg_group_id, tg_user=tg_user)
 
