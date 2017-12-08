4310f445701f6527e8344cf634772d0b9dcdcebb
jqqqqqqqqqq <jqqqqqqqqqq@qq.com>
2017-12-08 11:21:16 +0800

    fix dice & rps

--- plugins/for_fun.py
+++ plugins/for_fun.py
@@ -13,7 +13,7 @@ def dice(tg_group_id: int, tg_user: telegram.User, tg_message_id: int):
 
     reply_entity = list()
     reply_entity.append({'data': {'text': 'threw a dice'}, 'type': 'text'})
-    reply_entity.append({'type': 'dice'})
+    reply_entity.append({'data': {'type': '1'}, 'type': 'dice'})
 
     send_all_except_current(forward_index, reply_entity, tg_group_id=tg_group_id, tg_user=tg_user)
 
@@ -26,7 +26,7 @@ def rps(tg_group_id: int, tg_user: telegram.User, tg_message_id: int):
 
     reply_entity = list()
     reply_entity.append({'data': {'text': 'played rock–paper–scissors'}, 'type': 'text'})
-    reply_entity.append({'type': 'rps'})
+    reply_entity.append({'data': {'type': '1'}, 'type': 'rps'})
 
     send_all_except_current(forward_index, reply_entity, tg_group_id=tg_group_id, tg_user=tg_user)
 
