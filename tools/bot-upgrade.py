71a63ef6891305fe367cb04d7fb64a39616fc4a4
家鳖大帝 <Z4HD@outlook.com>
2017-12-09 21:55:06 +0800

    Add useful tools to upgrade bot by git.

---
+++ tools/bot-upgrade.py
@@ -0,0 +1,15 @@
+import os
+
+print("""
+本升级脚本仅适用于使用JSON格式配置文件的用户，会执行git pull拉取代码并将bot_constant-json.py重命名。
+如果您使用py格式的配置文件，执行本脚本会使您丢失全部配置。
+""")
+i=input("键入[y]继续，[n]退出>")
+
+if i.upper()=='Y':
+    os.chdir('..')
+    os.system('git reset --hard && git pull')
+    os.rename('bot_constant-json.py','bot_constant')
+else:
+    print("什么都没有发生...")
+    exit()
