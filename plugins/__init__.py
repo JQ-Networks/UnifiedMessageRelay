2457a9a0ccf0130a4689874f33368df09bf7599c
Curtis Jiang <jqqqqqqqqqq@qq.com>
2017-12-16 20:38:53 +0800

    new feature: add group member/join group

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -3,7 +3,8 @@ import plugins.show_group_id
 import plugins.for_fun
 import plugins.alipay_redpack
 
-import plugins._000_water_meter_control
+import plugins._002_water_meter_control
+import plugins._000_admins
 import plugins._001_group_invite
 
 import plugins._1001_command
