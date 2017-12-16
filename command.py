6acd5cb92ea30135b956c68bb3457bcdf8fe12a3
Curtis Jiang <jqqqqqqqqqq@qq.com>
2017-12-16 20:56:56 +0800

    add debug log

--- command.py
+++ command.py
@@ -1,5 +1,6 @@
 import global_vars
 import logging
+from functools import wraps
 
 
 logger = logging.getLogger("CTBMain." + __name__)
@@ -18,8 +19,16 @@ class Command:
 
 def command_listener(command, short_command='', require_admin=False, tg_only=False, qq_only=False, description=''):
     def decorator(handler):
-        global_vars.append_command(Command(command, short_command, handler, require_admin, tg_only, qq_only, description))
-        logger.debug('Registering new command: ' + handler.__name__)
-        global_vars.create_variable(handler.__name__, handler)  # add command to global_vars, for cross-plugin access
-        return handler
+        logger.debug('Registering new command: ' + command + '(' + handler.__name__ + ')')
+
+        @wraps(handler)
+        def return_wrapper(*args, **kwargs):
+            logger.debug(command + '(' + handler.__name__ + ')' + ' called')
+            return handler(*args, **kwargs)
+
+        global_vars.append_command(Command(command, short_command, return_wrapper, require_admin, tg_only, qq_only, description))
+
+        global_vars.create_variable(handler.__name__, return_wrapper)  # add command to global_vars, for cross-plugin access
+        logger.debug(command + '(' + handler.__name__ + ') added to global_vars')
+        return return_wrapper
     return decorator
