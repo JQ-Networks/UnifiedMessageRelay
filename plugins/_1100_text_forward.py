8e9c7bea56adf859e4cc19a9f3da5233d0828243
Curtis Jiang <jqqqqqqqqqq@qq.com>
2017-12-18 01:02:07 +0800

    added qq & tg message id association and improved tg reply

--- plugins/_1100_text_forward.py
+++ plugins/_1100_text_forward.py
@@ -545,7 +545,7 @@ def handle_forward(context):
     # save message to database, using telegram message id as index
     for msg_id in tg_message_id_list:
         global_vars.mdb.append_message(context.get('message_id'), msg_id, forward_index, context.get('user_id'))
-    logger.debug(str(global_vars.mdb.db))
+    logger.debug(str(global_vars.mdb.db.items()))
     return ''
 
 
