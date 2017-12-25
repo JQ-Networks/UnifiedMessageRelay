63fb681b55f0f7a223d412cbc308fc2d9b133413
家鳖大帝 <Z4HD@outlook.com>
2017-12-25 20:51:23 +0800

    Use argparse to rewrite args determine (#17)
    * Untest init
    
    * log telegram Bot library
    
    *Untest*
    
    * [Add SIGTERM signal handle](https://github.com/jqqqqqqqqqq/coolq-telegram-bot/projects/1#card-5623865)
    
    * catch ModuleNotFoundError
    
    * catch ModuleNotFoundError
    
    * print Error to screen
    
    * fix TypeError
    
    * Add command help str
    
    * Update format
    
    * Revert "Update format"
    
    This reverts commit 2875415454a8be7b27e21bcf656b397b2d2ae57f.
    
    * Use RawDescriptionHelpFormatter
    
    * 尬
    
    * Use RawTextHelpFormatter
    
    * edit level in debug mode
    
    * formatting...
    
    del unused empty line

--- daemon.py
+++ daemon.py
@@ -1,21 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from bot_constant import *
-import global_vars
-
-from telegram.ext import Updater, CommandHandler
-
+import argparse
 import logging
-from logging.handlers import RotatingFileHandler
-from DaemonClass import Daemon
-import threading
-import sys
-from message_persistence import MessageDB
-import time
-from cqhttp import CQHttp
-
 import queue
-import utils
+import threading
+import time
+from logging.handlers import RotatingFileHandler
+
+from telegram.ext import CommandHandler, Updater
 
 # region log
 
@@ -34,13 +26,27 @@ logger_plugins = logging.getLogger("CTBPlugin")
 logger_plugins.setLevel(logging.DEBUG)
 logger.addHandler(rHandler)
 
-# log telegram
+# log telegram Bot library
 
-logger_telegram = logging.getLogger('telegram')
-logger_telegram.setLevel(logging.DEBUG)
+# via https://pypi.python.org/pypi/python-telegram-bot#logging
+logger_telegram = logging.getLogger()
+logger_telegram.setLevel(logging.INFO)
 logger_telegram.addHandler(rHandler)
 
 # endregion
+
+# load config
+try:
+    from bot_constant import *
+    import utils
+    import global_vars
+    from cqhttp import CQHttp
+    from DaemonClass import Daemon
+    from message_persistence import MessageDB
+except ImportError as e:
+    logger.addHandler(logging.StreamHandler())
+    logger.critical("Can't import %s, please check it again." % e.name)
+    exit(1)
 
 
 def error(bot, update, error):
@@ -86,32 +92,35 @@ class MainProcess(Daemon):
 
 
 def main():
+    argP = argparse.ArgumentParser(
+        description="QQ <-> Telegram Bot Framework & Forwarder", formatter_class=argparse.RawTextHelpFormatter)
+    cmdHelpStr = """
+start   - start bot as a daemon
+stop    - stop bot
+restart - restart bot
+run     - run as foreground Debug mode. every log will print to screen and log to file.
+"""
+    argP.add_argument("command", type=str, action="store",
+                      choices=['start', 'stop', 'restart', 'run'], help=cmdHelpStr)
     daemon = MainProcess('/tmp/coolq-telegram-bot.pid')
-    if len(sys.argv) == 2:
-        if 'start' == sys.argv[1]:
-            daemon.start()
-        elif 'stop' == sys.argv[1]:
-            daemon.stop()
-        elif 'restart' == sys.argv[1]:
-            daemon.restart()
-        elif 'run' == sys.argv[1]:
-            logger.setLevel(logging.DEBUG)
-            logger_plugins.setLevel(logging.DEBUG)
-            sH=logging.StreamHandler()
-            sH.setFormatter(standardFormatter)
-            logger.addHandler(sH)
-            logger_plugins.addHandler(sH)
-            logger_telegram.addHandler(sH)
-            logger.info('Now running in debug mode...')
-            daemon.run()
-        else:
-            logger.error('error processing command')
-            print("usage: %s start|stop|restart|run" % sys.argv[0])
-            sys.exit(2)
-        sys.exit(0)
-    else:
-        print("usage: %s start|stop|restart|run" % sys.argv[0])
-        sys.exit(2)
+    args = argP.parse_args()
+    if args.command == 'start':
+        daemon.start()
+    elif args.command == 'stop':
+        daemon.stop()
+    elif args.command == 'restart':
+        daemon.restart()
+    elif args.command == 'run':
+        logger.setLevel(logging.DEBUG)
+        logger_plugins.setLevel(logging.DEBUG)
+        logger_telegram.setLevel(logging.DEBUG)
+        sH = logging.StreamHandler()
+        sH.setFormatter(standardFormatter)
+        logger.addHandler(sH)
+        logger_plugins.addHandler(sH)
+        logger_telegram.addHandler(sH)
+        logger.info('Now running in debug mode...')
+        daemon.run()
 
 
 if __name__ == '__main__':
