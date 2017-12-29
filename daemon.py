47050a11a6041241b29198fa1110a7b4b3671509
Curtis Jiang <jqqqqqqqqqq@qq.com>
2017-12-29 21:46:38 +0800

    colored logger

--- daemon.py
+++ daemon.py
@@ -12,12 +12,65 @@ from telegram.ext import CommandHandler, Updater
 
 # region log
 
+BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE = range(8)
+
+# The background is set with 40 plus the number of the color, and the foreground with 30
+
+# These are the sequences need to get colored ouput
+RESET_SEQ = "\033[0m"
+COLOR_SEQ = "\033[1;%dm"
+BOLD_SEQ = "\033[1m"
+
+
+def formatter_message(message, use_color = True):
+    if use_color:
+        message = message.replace("$RESET", RESET_SEQ).replace("$BOLD", BOLD_SEQ)
+    else:
+        message = message.replace("$RESET", "").replace("$BOLD", "")
+    return message
+
+COLORS = {
+    'WARNING': YELLOW,
+    'INFO': WHITE,
+    'DEBUG': BLUE,
+    'CRITICAL': YELLOW,
+    'ERROR': RED
+}
+
+
+class ColoredFormatter(logging.Formatter):
+    def __init__(self, msg, use_color=True):
+        logging.Formatter.__init__(self, msg)
+        self.use_color = use_color
+
+    def format(self, record):
+        levelname = record.levelname
+        if self.use_color and levelname in COLORS:
+            levelname_color = COLOR_SEQ % (30 + COLORS[levelname]) + levelname + RESET_SEQ
+            record.levelname = levelname_color
+        return logging.Formatter.format(self, record)
+
+
+class ColoredLogger(logging.Logger):
+    FORMAT = "[$BOLD%(name)-20s$RESET][%(levelname)-18s] ($BOLD%%(module)s(filename)s$RESET:%(lineno)d): %(message)s"
+    COLOR_FORMAT = formatter_message(FORMAT, True)
+
+    def __init__(self, name):
+        logging.Logger.__init__(self, name, logging.DEBUG)
+
+        color_formatter = ColoredFormatter(self.COLOR_FORMAT)
+
+        console = logging.StreamHandler()
+        console.setFormatter(color_formatter)
+
+        self.addHandler(console)
+        return
+
+
 # rotate file handler: max size: 1MB, so always enable debug mode is ok
 rHandler = RotatingFileHandler(
     'bot.log', maxBytes=1048576, backupCount=3)
-standardFormatter = logging.Formatter(
-    "%(asctime)s [%(levelname)s] %(name)s - %(module)s(%(filename)s) : %(message)s")
-rHandler.setFormatter(standardFormatter)
+logging.setLoggerClass(ColoredLogger)
 
 # log main thread
 logger = logging.getLogger("CTBMain")
