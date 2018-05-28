134243d35b2d75ab88d855104db9cf749e61ece7
Curtis Jiang <jqqqqqqqqqq@qq.com>
2018-05-28 09:57:20 +0800

    Update alipay_redpack.py

--- plugins/alipay_redpack.py
+++ plugins/alipay_redpack.py
@@ -14,7 +14,7 @@ def show_red_pack(forward_index: int,
                   qq_discuss_id: int=None,
                   qq_user: int=None):
 
-    message = '点击链接或者复制到浏览器打开，领取每日红包 https://qr.alipay.com/c1x00417d1veog0b99yea4e'
+    message = '点击链接或者复制到浏览器打开，领取每日红包（两个）\nhttps://jqqqqqqqqqq.cn/hongbao\nhttps://jogle.top/hb/mengniu'
 
     return send_both_side(forward_index,
                           message,
